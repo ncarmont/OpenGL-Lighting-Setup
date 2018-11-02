923d7012817e5f237fcb52c1a8896398f06d97e4
Nicolas Carmont <nickcarmont8@gmail.com>
2018-11-02 03:19:27 -0700

    elon musk head rot

--- GLFW openGL/main.cpp
+++ GLFW openGL/main.cpp
@@ -7,6 +7,12 @@
 //GLFW
 #include <GLFW/glfw3.h>
 
+#include "SOIL2/SOIL2.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Shader.h"
 
 const GLint WIDTH = 800, HEIGHT = 600;
@@ -44,45 +50,105 @@ int main(){
     // Define the viewport dimensions
     glViewport(0,0, screenWidth, screenHeight);
     
+    glEnable( GL_BLEND);
+    
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+
+    // Build and compile our shader program
     Shader ourShader ("resources/shaders/core.vs", "resources/shaders/core.frag");
-    
-    
     
     GLfloat vertices[]=
     {
-         // position             // color
-        -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,  // bottom left
-        0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f,  // bottom right
-        0.0f, 0.5f, 0.0f,       0.0f, 0.0f, 1.0f,  // middle top
+         // position             // color           // texture coordinates
+       0.5f, 0.32f, 0.0f,         1.0f, 0.0f,0.0f,   1.0f, 1.0f,  // Top Right
+       0.5f, -0.32f, 0.0f,        1.0f, 1.0f, 1.0f,  1.0f, 0.0f,  // Bottom Right
+      -0.5f, -0.32f, 0.0f,        1.0f, 0.0f, 0.0f,  0.0f, 0.0f,   // Bottom Left
+        -0.5f, 0.32f,0.0f,        1.0f, 0.0f, 1.0f,  0.0f, 1.0f,   // Top Left
     };
     
-    GLuint VBO, VAO;
+    GLuint indices[] = {
+        0, 1, 3, // First Triangle
+        1, 2, 3, // Second Triangle
+        
+    };
+    
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers( 1, &EBO );
     
+    // Bind the Vertex Array Objectv first, then bind and set vertex buffer(s) and attribute pointer(s).
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices, GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), (GLvoid *) 0 );
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices, GL_STATIC_DRAW);
+    
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(GLfloat), (GLvoid *) 0 );
     glEnableVertexAttribArray(0);
     
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), (GLvoid *) (3*sizeof(GLfloat)));
+    // Color Attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(GLfloat), (GLvoid *) (3*sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
     
-    glBindVertexArray(0);
+//    Texture coordinates attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(GLfloat), (GLvoid *) (6*sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+    
+    glBindVertexArray(0); //unbind VAO
+    
+    GLuint texture;
+    int width, height;
+    glGenTextures(1,&texture);
+    glBindTexture(GL_TEXTURE_2D, texture );
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    unsigned char *image = SOIL_load_image("resources/images/elon.png", &width, &height, 0, SOIL_LOAD_RGBA);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
     
     
+    // Game Loop
     while( !glfwWindowShouldClose(window) ){
+        
+        // Check if any events have been activated (key pressed, mouse, etc).
         glfwPollEvents();
         
+        // Render
+        // Clear the colorbuffer
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear( GL_COLOR_BUFFER_BIT);
         
+        // Draw the triangle
         ourShader.Use();
+        
+        
+        glm::mat4 transform(1);
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        transform = glm::rotate(transform, (GLfloat)glfwGetTime()*-5.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+        
+        GLint transformLocation = glGetUniformLocation( ourShader.Program,  "transform");
+        glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transform));
+        
+        
+        glActiveTexture( GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glUniform1i(glGetUniformLocation(ourShader.Program,"ourTexture"),0);
+        
+        
         glBindVertexArray( VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
         
         // Swap the screen buffers
@@ -90,6 +156,7 @@ int main(){
     }
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     
     // Termionate GLFW, clearing any resources allocated to GLFW
     glfwTerminate();
